5634fb83dd980b13ef113064cc2788cf2d9d81b3
kindesskay <kindenessudubamo@gmail.com>
2024-02-25 17:07:36 +0100

    new api proxy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://random-project-api.vercel.app/api",
+        target: "https://random-project-api.vercel.app/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
